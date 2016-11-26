7fa1f6b713c28440694b90722bdaa15496b53736
Nate Jones <n8jones@gmail.com>
2016-11-26 10:20:53 -0500

    Adding unit test for getChecksum

--- src/test/java/me/natejones/fc/FindDuplicatesTest.java
+++ src/test/java/me/natejones/fc/FindDuplicatesTest.java
@@ -1,8 +1,12 @@
 package me.natejones.fc;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.not;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
+import java.nio.file.Paths;
 import java.security.MessageDigest;
 import java.util.Collection;
 
@@ -20,4 +24,17 @@ public class FindDuplicatesTest {
 		assertTrue("pair node1", pair.getNode1().getName().endsWith("A.txt"));
 	}
 
+	@Test
+	public void test_checksum() throws Exception {
+		MessageDigest md = MessageDigest.getInstance("MD5");
+		IFileNode a =
+				new PathFileNode(Paths.get("./src/test/resources/A.txt"), md);
+		IFileNode a2 =
+				new PathFileNode(Paths.get("./src/test/resources/A2.txt"), md);
+		assertThat("checksum equals", a.getChecksum(), equalTo(a2.getChecksum()));
+		IFileNode b =
+				new PathFileNode(Paths.get("./src/test/resources/B.txt"), md);
+		assertThat("checksum not equals", a.getChecksum(),
+				not(equalTo(b.getChecksum())));
+	}
 }
